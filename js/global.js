f550cff45e57af3bb01d64685e05ad1da2be9f87
Aayush Gupta <aayushnj@gmail.com>
2020-08-02 23:57:44 -0400

    Meet the team page images + individual pages

--- js/global.js
+++ js/global.js
@@ -1,6 +1,8 @@
 $(function () {
   $("body").fadeIn();
-  if (navigator.userAgent.indexOf('Mac') > 0)
+  if (navigator.userAgent.indexOf('Mac') > 0) {
     $('#main-logo').css("top", "5px");
+    $("#header h1").css("padding-top", "12px")
+  }
 });
 
