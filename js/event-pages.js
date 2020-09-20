987ddedd3434ca5765aa7d975ef09c9ac27ea014
Aayush Gupta <aayushnj@gmail.com>
2020-09-20 14:16:04 -0400

    new update

---
+++ js/event-pages.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+  $('.single-item').slick({
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 1,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: true,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+});
--- js/individual-pages.js
+++ js/individual-pages.js
@@ -1,6 +1,10 @@
 $(window).load(function () {
   $('.flexslider').flexslider({
     animation: "slide",
+    slideshow: true,
+    slideshowSpeed: 7000,
+    touch: true,
+    keyboard: true,
     // controlNav: "thumbnails"
   });
 });
