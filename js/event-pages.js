06551e29e07c77824922c7b5c9ffcee0b0c18dba
Aayush Gupta <aayushnj@gmail.com>
2020-09-22 21:59:38 -0400

    home page finished

--- js/event-pages.js
+++ js/event-pages.js
@@ -1,8 +1,14 @@
 $(document).ready(function () {
   $('.single-item').slick({
     centerMode: true,
-    centerPadding: '60px',
-    slidesToShow: 1,
+    centerPadding: '0',
+    slidesToShow: 3,
+    // fade: true,
+    autoplay: false,
+    autoplaySpeed: 2500,
+    dots: true,
+    infinite: true,
+    focusOnSelect: true,
     responsive: [
       {
         breakpoint: 768,
@@ -10,13 +16,13 @@ $(document).ready(function () {
           arrows: true,
           centerMode: true,
           centerPadding: '40px',
-          slidesToShow: 3
+          slidesToShow: 1
         }
       },
       {
         breakpoint: 480,
         settings: {
-          arrows: false,
+          arrows: true,
           centerMode: true,
           centerPadding: '40px',
           slidesToShow: 1
@@ -24,4 +30,10 @@ $(document).ready(function () {
       }
     ]
   });
+  // var video = $('#single-item').find('video').get(0).play();
+
+  // $('#single-item').on('afterChange', function (event, slick, currentSlide, nextSlide) {
+  //   $('#single-item').find('video').get(0).pause();
+  //   var video = $('#single-item').find('video').get(0).play();
+  // });
 });
