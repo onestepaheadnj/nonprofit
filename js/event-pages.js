7d8da0e16d9805c019dc5617d675462028263e13
Aayush Gupta <aayushnj@gmail.com>
2020-09-23 00:42:54 -0400

    roles updated

--- js/event-pages.js
+++ js/event-pages.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-  $('.single-item').slick({
+  $('.center').slick({
     centerMode: true,
     centerPadding: '0',
     slidesToShow: 3,
@@ -30,10 +30,34 @@ $(document).ready(function () {
       }
     ]
   });
-  // var video = $('#single-item').find('video').get(0).play();
-
-  // $('#single-item').on('afterChange', function (event, slick, currentSlide, nextSlide) {
-  //   $('#single-item').find('video').get(0).pause();
-  //   var video = $('#single-item').find('video').get(0).play();
-  // });
+  $('.single-item').slick({
+    centerMode: true,
+    centerPadding: '48px',
+    slidesToShow: 1,
+    // fade: true,
+    autoplay: false,
+    autoplaySpeed: 2500,
+    infinite: true,
+    focusOnSelect: true,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: true,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: true,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
 });
