a23706314cd08c8e98b42f6fa46887068586657e
Aayush Gupta <aayushnj@gmail.com>
2020-09-25 09:57:54 -0400

    vid

--- js/event-pages.js
+++ js/event-pages.js
@@ -1,71 +1,81 @@
-const tags = document.querySelectorAll('[data-tabe-target]')
 
+
+// $(document).ready(function () {
+//   $('.center').slick({
+//     centerMode: true,
+//     centerPadding: '0px',
+//     slidesToShow: 3,
+//     // fade: true,
+//     autoplay: false,
+//     autoplaySpeed: 2500,
+//     dots: true,
+//     infinite: true,
+//     focusOnSelect: true,
+//     responsive: [
+//       {
+//         breakpoint: 768,
+//         settings: {
+//           arrows: true,
+//           centerMode: true,
+//           centerPadding: '0px',
+//           slidesToShow: 1
+//         }
+//       },
+//       {
+//         breakpoint: 480,
+//         settings: {
+//           arrows: true,
+//           centerMode: true,
+//           centerPadding: '40px',
+//           slidesToShow: 1
+//         }
+//       }
+//     ]
+//   });
+//   $('.single-item').slick({
+//     centerMode: true,
+//     centerPadding: '0px',
+//     slidesToShow: 1,
+//     // fade: true,
+//     autoplay: false,
+//     autoplaySpeed: 2500,
+//     infinite: true,
+//     focusOnSelect: true,
+//     responsive: [
+//       {
+//         breakpoint: 768,
+//         settings: {
+//           arrows: true,
+//           centerMode: true,
+//           // centerPadding: '80px',
+//           slidesToShow: 1
+//         }
+//       },
+//       {
+//         breakpoint: 480,
+//         settings: {
+//           arrows: true,
+//           centerMode: true,
+//           // centerPadding: '40px',
+//           slidesToShow: 1
+//         }
+//       }
+//     ]
+//   });
+// });
+
+const tags = document.querySelectorAll('[data-tab-target]')
+const tabContents = document.querySelectorAll('[data-tab-content]')
 tabs.forEach(tab => {
   tab.addEventListener('click', () => {
     const target = document.querySelector(tab.dataset.tabTarget)
-    target.classList('active-tab')
+    tabContents.forEach(tabContent => {
+      tabContent.classList.remove('active')
+    })
+    tabs.forEach(tab => {
+      tab.classList.remove('active')
+    })
+    tab.classList.add('active')
+    target.classList.add('active-tab')
   })
 })
-$(document).ready(function () {
-  $('.center').slick({
-    centerMode: true,
-    centerPadding: '0px',
-    slidesToShow: 3,
-    // fade: true,
-    autoplay: false,
-    autoplaySpeed: 2500,
-    dots: true,
-    infinite: true,
-    focusOnSelect: true,
-    responsive: [
-      {
-        breakpoint: 768,
-        settings: {
-          arrows: true,
-          centerMode: true,
-          centerPadding: '0px',
-          slidesToShow: 1
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          arrows: true,
-          centerMode: true,
-          centerPadding: '40px',
-          slidesToShow: 1
-        }
-      }
-    ]
-  });
-  $('.single-item').slick({
-    centerMode: true,
-    centerPadding: '0px',
-    slidesToShow: 1,
-    // fade: true,
-    autoplay: false,
-    autoplaySpeed: 2500,
-    infinite: true,
-    focusOnSelect: true,
-    responsive: [
-      {
-        breakpoint: 768,
-        settings: {
-          arrows: true,
-          centerMode: true,
-          // centerPadding: '80px',
-          slidesToShow: 1
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          arrows: true,
-          centerMode: true,
-          // centerPadding: '40px',
-          slidesToShow: 1
-        }
-      }
-    ]
-  });
-});
