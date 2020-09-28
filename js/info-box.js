5ccea7671a6d7536b3cacfe0b0791087d0082649
Aayush Gupta <aayushnj@gmail.com>
2020-09-28 19:29:10 -0400

    fix

---
+++ js/info-box.js
@@ -0,0 +1,15 @@
+const tabs = document.querySelectorAll('[data-tab-target]')
+const tabContents = document.querySelectorAll('[data-tab-content]')
+tabs.forEach(tab => {
+  tab.addEventListener('click', () => {
+    const target = document.querySelector(tab.dataset.tabTarget)
+    tabContents.forEach(tabContent => {
+      tabContent.classList.remove('actives')
+    })
+    tabs.forEach(tab => {
+      tab.classList.remove('actives')
+    })
+    tab.classList.add('actives')
+    target.classList.add('actives-tab')
+  })
+})
---
+++ js/slider.js
@@ -0,0 +1,63 @@
+$(document).ready(function () {
+  $('.center').slick({
+    centerMode: true,
+    centerPadding: '0px',
+    slidesToShow: 3,
+    // fade: true,
+    autoplay: false,
+    autoplaySpeed: 2500,
+    dots: true,
+    infinite: true,
+    focusOnSelect: true,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: true,
+          centerMode: true,
+          centerPadding: '0px',
+          slidesToShow: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: true,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+  $('.single-item').slick({
+    centerMode: true,
+    centerPadding: '0px',
+    slidesToShow: 1,
+    // fade: true,
+    autoplay: false,
+    autoplaySpeed: 2500,
+    infinite: true,
+    focusOnSelect: true,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: true,
+          centerMode: true,
+          // centerPadding: '80px',
+          slidesToShow: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: true,
+          centerMode: true,
+          // centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+});
